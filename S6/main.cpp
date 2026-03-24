61e52ffcbf8d4de6aa2f2e370c4ab91945c67383
Amos Brocco <contact@amosbrocco.ch>
2026-03-24 15:04:24 +0100

    End 24.3

--- S6/main.cpp
+++ S6/main.cpp
@@ -1,5 +1,81 @@
 #include <iostream>
 
+void print(int i, const std::string &header = "--- value ---") {
+  std::cout << header << std::endl;
+  std::cout << i << std::endl;
+}
+
+struct Printer {
+  void print(int i) {
+    std::cout << m_header << std::endl;
+    std::cout << i << std::endl;
+  }
+
+  void header(const std::string &header) { m_header = header; }
+
+  void operator()(int i) { print(i); }
+
+private:
+  std::string m_header{"--- value ---"};
+};
+
+// Functor
+struct IsPositiveNumber {
+  bool operator()(int i) const { return i >= 0; }
+};
+
+// With a functor
+void check(int i, IsPositiveNumber pn) {
+  if (pn(i)) {
+    // ...
+  }
+}
+
+// With a function pointer
+void check(int i, bool (*is_positive_number)(int)) {
+  if (is_positive_number(i)) {
+    // ...
+  }
+}
+
 int main() {
-    std::cout << "Hello world\n";
+  print(42);
+  print(77);
+  print(8, "--- valore --");
+
+  // Using the Printer class
+  Printer p;
+  p.print(42);
+  p.print(77);
+  Printer p2;
+  p2.header("--- valore ---");
+  p2.print(8);
+
+  p(42);
+  p(77);
+  p2(8);
+
+  std::cout << " ---- with lambdas -----\n";
+
+  auto h = "--- value ---";
+  auto f = "--- ---";
+  // auto p3 = [h,f](int i) -> void {
+  auto p3 = [=](int i) -> void {
+    std::cout << h << std::endl;
+    std::cout << i << std::endl;
+    std::cout << f << std::endl;
+  };
+  h = "*******";
+  p3(42);
+  p3(77);
+  auto p4 = [&, f](int i) {
+    double pi{3.14};
+    std::cout << h << std::endl;
+    std::cout << i << std::endl;
+    std::cout << f << std::endl;
+  };
+
+  /*auto c = [counter = 0]() { return ++counter; };
+  p4(42);
+  p4(77);*/
 }
