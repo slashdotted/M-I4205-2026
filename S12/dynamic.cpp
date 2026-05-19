eef68b1cd28fc497c058efe98be04ee783feedad
Amos Brocco <contact@amosbrocco.ch>
2026-05-19 14:25:15 +0200

    End 19.5

--- S12/dynamic.cpp
+++ S12/dynamic.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Base {
 public:
-  void speak() { cout << "Base::speak" << endl; }
+  virtual void speak() { cout << "Base::speak" << endl; }
 };
 
 class Derived : public Base {
@@ -14,16 +14,50 @@ private:
   double m_pi{3.1415};
 };
 
+bool isDerived(void *ptr) {
+  // void is not a polymorphic class
+  /*if (dynamic_cast<Derived *>(ptr)) {
+    return true;
+  }*/
+  return false;
+}
+
+// "instanceof Derived"
+bool isDerived(Base *ptr) {
+  if (dynamic_cast<Derived *>(ptr)) {
+    return true;
+  }
+  return false;
+}
+
+// "instanceof Derived"
+bool isDerived(Base &b) {
+  try {
+    auto t = dynamic_cast<Derived &>(b);
+    return true;
+  } catch (...) {
+    return false;
+  }
+}
+
 int main() {
   {
     Base *b1{new Derived()};
     Base *b2{new Base()};
 
-    Derived *d1 = static_cast<Derived *>(b1);
+    Derived *d1 = dynamic_cast<Derived *>(b1);
     d1->onlyDerived();
 
-    Derived *d2 = static_cast<Derived *>(b2);
-    d2->onlyDerived();
+    Derived *d2 = dynamic_cast<Derived *>(b2);
+    // if the cast is not valid we get nullptr
+    if (d2) { // check if d2 is not nullptr
+      d2->onlyDerived();
+    }
+
+    // Emulating instance of
+    if (dynamic_cast<Derived *>(b2)) {
+      // "b2 instanceof Derived"
+    }
 
     delete b1;
     delete b2;
@@ -32,11 +66,15 @@ int main() {
     Derived b1;
     Base b2;
 
-    Derived &d1 = static_cast<Derived &>(b1);
+    Derived &d1 = dynamic_cast<Derived &>(b1);
     d1.onlyDerived();
 
-    Derived &d2 = static_cast<Derived &>(b2);
-    d2.onlyDerived();
+    try {
+      Derived &d2 = dynamic_cast<Derived &>(b2);
+      d2.onlyDerived();
+    } catch (const std::bad_cast &e) {
+      // Invalid cast
+    }
   }
   return 0;
 }
--- S12/dynamic_vbase.cpp
+++ S12/dynamic_vbase.cpp
@@ -1,14 +1,14 @@
 class A {
 public:
-    virtual ~A() {}
+  virtual ~A() {}
 };
 
-class B : public A {};
-class C : public A {};
+class B : public virtual A {};
+class C : public virtual A {};
 class D : public B, public C {};
 
 int main() {
-    B b;
-    A* a{&b};
-    B* c{static_cast<B*>(a)};
+  B b;
+  A *a{&b};
+  B *c{dynamic_cast<B *>(a)};
 }
